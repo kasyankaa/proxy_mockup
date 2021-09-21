a19ac223b1984aed35963b54fffc8d3198d7b70d
Christina Kasyan <55959819+kasyankaa@users.noreply.github.com>
2021-09-21 12:58:16 +0300

    Modified rf for denied address

--- security-sf-v1/sharedflowbundle/resources/jsc/JavaScript-1.js
+++ security-sf-v1/sharedflowbundle/resources/jsc/JavaScript-1.js
@@ -2,6 +2,6 @@ var listOfDeniedIps = context.getVariable("ip")
 var clientIp = context.getVariable("request.header.X-Forwarded-For")
 
 if (listOfDeniedIps.includes(clientIp)){
-    context.setVariable("triggerError", "true");
+    throw "Access Denied";
 }
 
